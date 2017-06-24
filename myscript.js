5dfaa18feb4f917d7cf35bc083c158cf0b7df76f
Marc <marc82490@gmail.com>
2017-06-24 11:12:47 -0500

    Updated clone

--- myscript.js
+++ myscript.js
@@ -1,35 +1,35 @@
-//credit goes to Steven Frank of Cloud to Butt (https://github.com/panicsteve/cloud-to-butt/)
+//
 
 walk(document.body);
 
-function walk(node)  
+function walk(node)
 {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
-	
+
 	var child, next;
 
-	switch ( node.nodeType )  
+	switch ( node.nodeType )
 	{
-		case 1: 
-		case 9:  
-		case 11: 
+		case 1:
+		case 9:
+		case 11:
 			child = node.firstChild;
-			while ( child ) 
+			while ( child )
 			{
-				next = child.nextSibling; 
+				next = child.nextSibling;
 				walk(child);
 				child = next;
 			}
 			break;
 
-		case 3: 
+		case 3:
 			handleText(node);
 			break;
 	}
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
@@ -39,6 +39,6 @@ function handleText(textNode)
 	v = v.replace(/\bScones\b/g, "Caaaaarbs");
 	v = v.replace(/\bBuns\b/g, "Caaaaarbs");
 	v = v.replace(/\bspaghetti\b/g, "caaaaarbs");
-	
+
 	textNode.nodeValue = v;
 }
